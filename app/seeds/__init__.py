a50dc4ff8945fdfa5352b8f9d875da8254660898
Collin <collinullmann@gmail.com>
2024-03-06 17:36:03 -0500

    added undo in seeder init before seeding on render

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -22,7 +22,13 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_deck_matches()
+        undo_matches()
+        undo_deck_cards()
+        undo_events()
         undo_users()
+        undo_cards()
+        undo_decks()
     seed_users()
     seed_cards()
     seed_decks()
