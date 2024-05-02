deb5667d7ee929d38b873d178308527aa951c5e0
Collin Ullmann <collinullmann@gmail.com>
2024-05-02 11:37:21 -0400

    added decklistCardPreview tooltip

--- react-vite/src/components/DeckComponents/DecklistPanel/CardTile.jsx
+++ react-vite/src/components/DeckComponents/DecklistPanel/CardTile.jsx
@@ -3,20 +3,25 @@ import './CardTile.css'
 
 export function CardTile({ card, count }) {
 
+
   let key = 0;
   return (
-    <div className="card-tile-div">
-      <div style={{margin:'0 5px'}}>
-        <div className="card-tile-image-div" style={{backgroundImage: `url(${card.imageUrl})`, backgroundPositionX: '50%', backgroundPositionY: '25%', }}>
+    <div>
+      <div className="card-tile-div">
+        <div style={{margin:'0 5px'}}>
+          <div className="card-tile-image-div" style={{backgroundImage: `url(${card.imageUrl})`, backgroundPositionX: '50%', backgroundPositionY: '25%', }}>
 
+        </div>
+        </div>
+        <div className="card-tile-name-manacost-div">
+          <p className="card-tile-name">{card.name}</p>
+          <p className="card-tile-manacost">{card.manaCost.length > 0 ? card.manaCost.split('}{').map(symbol => symbol.replace('{', '').replace('}', '')).map(symbol => `{${symbol}}`).map(symbol => <ManaSymbol key={key++} symbol={symbol}></ManaSymbol>) : null}</p>
+        </div>
+        <div className="card-tile-count-div">
+          <p className="card-tile-count">{count}</p>
+        </div>
       </div>
-      </div>
-      <div className="card-tile-name-manacost-div">
-        <p className="card-tile-name">{card.name}</p>
-        <p className="card-tile-manacost">{card.manaCost.length > 0 ? card.manaCost.split('}{').map(symbol => symbol.replace('{', '').replace('}', '')).map(symbol => `{${symbol}}`).map(symbol => <ManaSymbol key={key++} symbol={symbol}></ManaSymbol>) : null}</p>
-      </div>
-      <div className="card-tile-count-div">
-        <p className="card-tile-count">{count}</p>
+      <div className='decklistCardPreview decklistPreviewActive' style={{backgroundImage: `url(${card.imageUrl})`, backgroundSize: 'cover'}}>
       </div>
     </div>
   )
