bc0ca92a683f59e4af6a0bcddbb9c115a14353e2
Collin Ullmann <collinullmann@gmail.com>
2024-05-02 15:11:22 -0400

    added loading spinner to event creation and deletion

--- react-vite/src/components/EventComponents/EventDetailsPage/DeleteEventConfirmationModal.jsx
+++ react-vite/src/components/EventComponents/EventDetailsPage/DeleteEventConfirmationModal.jsx
@@ -2,16 +2,18 @@ import { useDispatch, useSelector } from "react-redux"
 // import { thunkDeleteMatch, thunkFetchMatchById } from "../../../redux/match";
 import { useModal } from "../../../context/Modal";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { thunkDeleteEvent, thunkFetchEventById } from "../../../redux/event";
 
-
+import '../../LoadingSpinnerModal.css'
 
 
 export function DeleteEventConfirmationModal({ eventId }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-const { closeModal } = useModal();
+  const { closeModal } = useModal();
+  const [loading, setLoading] = useState(false)
+
   const currentUser = useSelector(state => state.session.user)
 
   useEffect(() => {
@@ -23,18 +25,25 @@ const { closeModal } = useModal();
 
 
 
-  function handleDelete() {
-    dispatch(thunkDeleteEvent(eventId)).then(() => navigate(`/users/${currentUser.id}`)).then(() => closeModal())
+  async function handleDelete(e) {
+    e.preventDefault()
+    setLoading(true)
+    dispatch(thunkDeleteEvent(eventId)).then(() => setLoading(false)).then(() => navigate(`/users/${currentUser.id}`)).then(() => closeModal())
   }
 
   return (
-    <div style={{display:'flex', flexDirection:'column', alignItems:'center', margin:'20px'}}>
-      <h1>Deleting &quot;{event?.name}&quot;</h1>
-      <p style={{marginBottom:40}}>Are you sure?</p>
-      <div style={{display:'flex', columnGap:'12px'}}>
-        <button className="delete-confirmation-yes button standard" onClick={handleDelete}>Yes</button>
-        <button className="delete-confirmation-no button standard" onClick={closeModal}>Cancel</button>
+    <>
+      <div className={loading ? "loadingDiv loading" : "loadingDiv"} >
+        <div class="lds-dual-ring"></div>
       </div>
-    </div>
+      <div style={{display:'flex', flexDirection:'column', alignItems:'center', margin:'20px'}}>
+        <h1>Deleting &quot;{event?.name}&quot;</h1>
+        <p style={{marginBottom:40}}>Are you sure?</p>
+        <div style={{display:'flex', columnGap:'12px'}}>
+          <button className="delete-confirmation-yes button standard" onClick={handleDelete}>Yes</button>
+          <button className="delete-confirmation-no button standard" onClick={closeModal}>Cancel</button>
+        </div>
+      </div>
+    </>
   )
 }
--- react-vite/src/components/EventComponents/EventFormModal/EventFormModal.jsx
+++ react-vite/src/components/EventComponents/EventFormModal/EventFormModal.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { thunkCreateEvent, thunkFetchEventById, thunkUpdateEvent } from "../../../redux/event";
-
+import '../../LoadingSpinnerModal.css'
 import './EventFormModal.css'
 import { useModal } from "../../../context/Modal";
 
@@ -16,6 +16,8 @@ export function EventFormModal({ formtype, eventId }) {
   const [name, setName] = useState();
   const [date, setDate] = useState();
   const [format, setFormat] = useState();
+
+  const [loading, setLoading] = useState(false)
 
   const { closeModal } = useModal();
 
@@ -56,72 +58,76 @@ export function EventFormModal({ formtype, eventId }) {
     setFormat(event.format)
   }, [eventId, eventsById])
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setLoading(true)
     const eventFormData = new FormData();
     eventFormData.append('name', name)
     eventFormData.append('date', date)
     eventFormData.append('format', format)
 
     if (formtype == 'update') {
-      dispatch(thunkUpdateEvent(eventId, eventFormData)).then(() => navigate(`/events/${eventId}`)).then(() => closeModal())
+      dispatch(thunkUpdateEvent(eventId, eventFormData)).then(() => setLoading(false)).then(() => navigate(`/events/${eventId}`)).then(() => closeModal())
     } else {
-      dispatch(thunkCreateEvent(eventFormData)).then(eventOrFailureMessage => {
+      dispatch(thunkCreateEvent(eventFormData)).then(() => setLoading(false)).then(eventOrFailureMessage => {
         if(typeof eventOrFailureMessage === 'object') {
-          closeModal();
-          navigate(`/events/${eventOrFailureMessage.id}`)
+          navigate(`/events/${eventOrFailureMessage.id}`).then(() => closeModal())
         }
       })
     }
   }
 
   return (
-    <div className="event-form-container">
-      <h1>Event Form</h1>
+    <>
+      <div className={loading ? "loadingDiv loading" : "loadingDiv"} >
+        <div class="lds-dual-ring"></div>
+      </div>
+      <div className="event-form-container">
+        <h1>Event Form</h1>
 
-      <form className="event-form" onSubmit={handleSubmit}>
-        <label className="event-form-input">
-          <span className="event-form-input-label-span">Name</span>
-          <input 
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-          />
-        </label>
-
-        <label className="event-form-input">
-          <span className="event-form-input-label-span">Date</span>
-          <input 
-          type="date" 
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          required
-          />
-        </label>
-
-        <label className="event-form-input">
-          <span className="event-form-input-label-span">Format</span>
-          <select 
-            name="format"
-            value={format}
-            onChange={(e) => setFormat(e.target.value)}
+        <form className="event-form" onSubmit={handleSubmit}>
+          <label className="event-form-input">
+            <span className="event-form-input-label-span">Name</span>
+            <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
-          >
-            <option className="event-form-format-option" value="">(Select One)</option>
-            <option className="event-form-format-option" value="Commander">Commander</option>
-            <option className="event-form-format-option" value="Modern">Modern</option>
-            <option className="event-form-format-option" value="Legacy">Legacy</option>
-          </select>
-        </label>
+            />
+          </label>
 
-        <div className="event-form-submit-button-div">
-          <button className="event-form-submit-button standard">Submit</button>
-          <button className="event-form-cancel-button standard" onClick={closeModal}>Cancel</button>
-        </div>
-      </form>
-    </div>
+          <label className="event-form-input">
+            <span className="event-form-input-label-span">Date</span>
+            <input 
+            type="date" 
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            />
+          </label>
+
+          <label className="event-form-input">
+            <span className="event-form-input-label-span">Format</span>
+            <select 
+              name="format"
+              value={format}
+              onChange={(e) => setFormat(e.target.value)}
+              required
+            >
+              <option className="event-form-format-option" value="">(Select One)</option>
+              <option className="event-form-format-option" value="Commander">Commander</option>
+              <option className="event-form-format-option" value="Modern">Modern</option>
+              <option className="event-form-format-option" value="Legacy">Legacy</option>
+            </select>
+          </label>
+
+          <div className="event-form-submit-button-div">
+            <button className="event-form-submit-button standard">Submit</button>
+            <button className="event-form-cancel-button standard" onClick={closeModal}>Cancel</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 
 }
