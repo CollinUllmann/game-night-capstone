32686591dedb81d50690af420b5a8699acc36e6a
Collin Ullmann <collinullmann@gmail.com>
2024-05-02 12:29:15 -0400

    added deckDeleteConfirmation loading spinner

--- react-vite/src/components/DeckComponents/DeckTile/DeleteDeckConfirmationModal.jsx
+++ react-vite/src/components/DeckComponents/DeckTile/DeleteDeckConfirmationModal.jsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux"
 import { thunkDeleteDeck } from "../../../redux/deck";
 import { useModal } from "../../../context/Modal";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import '../../LoadingSpinnerModal.css'
 
 
 
@@ -10,6 +12,8 @@ export function DeleteDeckConfirmationModal({ deckId }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { closeModal } = useModal();
+  const [loading, setLoading] = useState(false)
+
   const currentUser = useSelector(state => state.session.user)
 
 
@@ -18,17 +22,26 @@ export function DeleteDeckConfirmationModal({ deckId }) {
 
 
   function handleDelete() {
-    dispatch(thunkDeleteDeck(deckId)).then(() => navigate(`/users/${currentUser.id}`)).then(() => closeModal())
+    setLoading(true)
+    dispatch(thunkDeleteDeck(deckId)).then(() => setLoading(false)).then(() => navigate(`/users/${currentUser.id}`)).then(() => closeModal())
   }
 
   return (
-    <div style={{display:'flex', flexDirection:'column', alignItems:'center', margin:'20px'}}>
-      <h1>Deleting &quot;{deck?.name}&quot;</h1>
-      <p style={{marginBottom:40}}>Are you sure?</p>
-      <div style={{display:'flex', columnGap:'12px'}}>
-        <button className="delete-confirmation-yes button standard" onClick={handleDelete}>Yes</button>
-        <button className="delete-confirmation-no button standard" onClick={closeModal}>Cancel</button>
+    <>
+      <div className={loading ? "loadingDiv loading" : "loadingDiv"} >
+        <div class="lds-dual-ring"></div>
       </div>
-    </div>
+
+      <div style={{display:'flex', flexDirection:'column', alignItems:'center', margin:'20px'}}>
+        <h1>Deleting &quot;{deck?.name}&quot;</h1>
+        <p style={{marginBottom:40}}>Are you sure?</p>
+        <div style={{display:'flex', columnGap:'12px'}}>
+          <button className="delete-confirmation-yes button standard" onClick={handleDelete}>Yes</button>
+          <button className="delete-confirmation-no button standard" onClick={closeModal}>Cancel</button>
+        </div>
+      </div>
+    </>
+    
+
   )
 }
